e23fe94a2279053a377222540623e3c1f733f262
Jeffrey I. Schiller <jis@mit.edu>
2013-06-08 16:24:41 -0400

    MIT BRANDING COMMIT (mit-appinventor-newblocks)
    MIT BRANDING COMMIT: This commit sets up the MIT App Inventor 2 Alpha
    Release (formerly "newblocks") branding. This commit is *ONLY* for
    http://ai2.appinventor.mit.edu/ (app id mit-appinventor-newblocks)
    
    Change-Id: I9867dc59b6d87402f64df3e2ac02e783366a986a

--- appinventor/common/src/com/google/appinventor/common/version/AppInventorFeatures.java
+++ appinventor/common/src/com/google/appinventor/common/version/AppInventorFeatures.java
@@ -100,7 +100,7 @@ public final class AppInventorFeatures {
    * @return true to display the survey splash screen
    */
   public static boolean showSurveySplashScreen() {
-    return false;
+    return true;
   }
 
   /**
@@ -111,7 +111,7 @@ public final class AppInventorFeatures {
    * @return true to limit account use to one session at a time
    */
   public static boolean requireOneLogin() {
-    return false;
+    return true;
   }
 
   /**
