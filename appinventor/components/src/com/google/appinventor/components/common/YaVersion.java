e6e930670590d6fcd0458f57417304fb9bf8ab3a
Jeffrey I. Schiller <jis@mit.edu>
2014-02-11 22:36:27 -0500

    Setup Companion Updating
    Change-Id: Ice684a299688c833ecd12a568ccd0e5989911528

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -901,10 +901,10 @@ public class YaVersion {
   // key as the Companion it is replacing, as the Package Manager
   // is invoked from the running Companion.
 
-  public static final String PREFERRED_COMPANION = "2.26";
-  public static final String COMPANION_UPDATE_URL = "";
-  public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.26" };
+  public static final String PREFERRED_COMPANION = "2.26 or 2.27";
+  public static final String COMPANION_UPDATE_URL = "/companions/MITAI2Companion.asc";
+  public static final String COMPANION_UPDATE_URL1 = "/companions/MITAI2Companion.apk";
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.24", "2.25", "2.26", "2.27" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
