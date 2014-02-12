016f6fbf94d3df39956a739c584120d121de1355
Jeffrey I. Schiller <jis@mit.edu>
2014-02-11 22:36:27 -0500

    Setup Companion Updating
    Change-Id: Ice684a299688c833ecd12a568ccd0e5989911528

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1077,9 +1077,9 @@ public class YaVersion {
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
   public static final String PREFERRED_COMPANION = "2.42";
-  public static final String COMPANION_UPDATE_URL = "";
-  public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.42" };
+  public static final String COMPANION_UPDATE_URL = "/companions/MITAI2Companion.asc";
+  public static final String COMPANION_UPDATE_URL1 = "/companions/MITAI2Companion.apk";
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.40", "2.41", "2.42" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
