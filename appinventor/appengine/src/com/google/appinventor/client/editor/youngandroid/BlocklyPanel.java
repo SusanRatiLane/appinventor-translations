59e9a3ac23d8bff518d0fa350ab37e6d4a42da99
Jeffrey I. Schiller <jis@mit.edu>
2016-08-23 21:41:46 -0400

    Arrange to save Screen Shots of Blocks
    Arrange to make a screenshot of the blocks whenever a blocks editor is
    exited.
    
    Change-Id: I4f88aa8c479e6b111bdc477a0fb3821cc815be15

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/BlocklyPanel.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/BlocklyPanel.java
@@ -21,6 +21,7 @@ import com.google.appinventor.components.common.YaVersion;
 import com.google.appinventor.shared.rpc.project.ProjectServiceAsync;
 import com.google.common.collect.Maps;
 
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.core.client.JavaScriptObject;
 
@@ -661,6 +662,10 @@ public class BlocklyPanel extends HTMLPanel implements ComponentDatabaseChangeLi
     DesignToolbar.popScreen();
   }
 
+  public void getBlocksImage(Callback callback) {
+    doFetchBlocksImage(formName, callback);
+  }
+
   // The code below (4 methods worth) is for creating a GWT dialog box
   // from the blockly code. See the comment in replmgr.js for more
   // information.
@@ -1069,4 +1074,16 @@ public class BlocklyPanel extends HTMLPanel implements ComponentDatabaseChangeLi
   public static native void doVerifyAllBlocks(String formName) /*-{
       $wnd.Blocklies[formName].Component.verifyAllBlocks();
   }-*/;
+
+  public static native void doFetchBlocksImage(String formName, Callback<String,String> callback) /*-{
+      var callb = $entry(function(result, error) {
+          if (error) {
+             callback.@com.google.gwt.core.client.Callback::onFailure(Ljava/lang/Object;)(error);
+          } else {
+             callback.@com.google.gwt.core.client.Callback::onSuccess(Ljava/lang/Object;)(result);
+          }
+      });
+      $wnd.Blocklies[formName].ExportBlocksImage.getUri(callb);
+  }-*/;
+
 }
