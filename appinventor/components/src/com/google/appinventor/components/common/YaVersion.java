b1d37449b3af6d06a9f5e3a56f639f004f74532b
Justus <quixoticquixer@gmail.com>
2017-08-28 11:39:08 +0530

    Update Companion App Version and Update Acceptable Companion Versions

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1080,10 +1080,10 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.43";
+  public static final String PREFERRED_COMPANION = "2.44tasks";
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.43" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.44tasks" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
