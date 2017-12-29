745a0d20d712d4b77addb2ff1a791f0303edc5ff
Evan W. Patton <ewpatton@mit.edu>
2017-12-29 13:10:02 -0500

    Add CompanionUpgradeHelper to ReplMgr
    Emulator upgrades broke with 2.45 because the new companion is so
    large it triggers an OutOfMemoryError in the emulator. This change
    adds an app called CompanionUpgradeHelper. Its sole purpose is to
    download the Companion directly to disk within the emulator. It then
    runs the package installer to upgrade the Companion. In order to be
    launched by the web interface, it has the same package and version
    number as 2.45 Companion and replaces itself with the real companion
    on success.
    
    Change-Id: Id83460366b029ac2a4a9877be407013d202c25ec

--- appinventor/blocklyeditor/src/replmgr.js
+++ appinventor/blocklyeditor/src/replmgr.js
@@ -83,7 +83,8 @@ Blockly.ReplStateObj.prototype = {
     'rendezvous2' : 'http://rendezvous.appinventor.mit.edu/rendezvous2/',
     'iceservers' : { 'iceServers' : [ { 'urls' : ['turn:turn.appinventor.mit.edu:3478'],
                                         'username' : 'oh',
-                                        'credential' : 'boy' }]}
+                                        'credential' : 'boy' }]},
+    'emulator': false
 };
 
 // Blockly is only loaded once now, so we can init this here.
@@ -686,6 +687,7 @@ Blockly.ReplMgr.putYail = (function() {
                 if (this.readyState == 4 && this.status == 200) {
                     rs.didversioncheck = true;
                     if (this.response[0] != "{") {
+                        top.ReplState.needsUpgradeHelper = true;
                         engine.checkversionupgrade(true, "", true); // Old Companion
                         engine.resetcompanion();
                         return;
@@ -786,6 +788,8 @@ Blockly.ReplMgr.putYail = (function() {
 //   button.
 //          context.hardreset(context.formName); // kill adb and emulator
             rs.didversioncheck = false;
+            rs.emulator = false;
+            rs.needsUpgradeHelper = false;
             top.BlocklyPanel_indicateDisconnect();
             engine.reset();
         },
@@ -874,6 +878,8 @@ Blockly.ReplMgr.triggerUpdate = function() {
         rs.connection = null;
         rs.didversioncheck = false;
         rs.isUSB = false;
+        rs.emulator = false;
+        rs.needsUpgradeHelper = false;
         context.resetYail(false);
         top.BlocklyPanel_indicateDisconnect();
         // End reset companion state
