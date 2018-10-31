5751d3ec1f1e18f3e0193724cf6729a69918cfdf
Jeffrey I. Schiller <jis@mit.edu>
2018-10-30 22:44:12 -0400

    BRANDING: Setup Companion Updating for 2.47t10
    Change-Id: Idc85580925de9d0ed43236fb5c41ac038bd7f70b

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1190,11 +1190,11 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.47";
-  public static final String COMPANION_UPDATE_URL = "";
-  public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String COMPANION_UPDATE_EMULATOR_URL = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.47" };
+  public static final String PREFERRED_COMPANION = "2.47t10";
+  public static final String COMPANION_UPDATE_URL = "/companions/MITAI2Companion.asc";
+  public static final String COMPANION_UPDATE_URL1 = "/companions/MITAI2Companion.apk";
+  public static final String COMPANION_UPDATE_EMULATOR_URL = "/companions/UpdateEmulator.asc";
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.47t10" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 0;
