cd22ad468df0069c8e299228b92d6dd21ef1b5c0
Jeffrey I. Schiller <jis@mit.edu>
2019-08-23 15:52:31 -0400

    BRANDING: Update YaVersion for Compatibility
    Update YaVersion to accept older Companion versions
    
    Also add Companion Update URLS for production
    
    Change-Id: Ibaf9dc94b964a18ac55841466d601c500119d186

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1255,10 +1255,13 @@ public class YaVersion {
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
   public static final String PREFERRED_COMPANION = "2.55 or 2.55u";
-  public static final String COMPANION_UPDATE_URL = "";
-  public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String COMPANION_UPDATE_EMULATOR_URL = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.55", "2.55u" };
+  public static final String COMPANION_UPDATE_URL = "/companions/MITAI2Companion.asc";
+  public static final String COMPANION_UPDATE_URL1 = "/companions/MITAI2Companion.apk";
+  public static final String COMPANION_UPDATE_EMULATOR_URL = "/companions/CompanionUpgradeHelper.asc";
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.52", "2.52u",
+                                                          "2.53", "2.53u",
+                                                          "2.54", "2.54u",
+                                                          "2.55", "2.55u" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
